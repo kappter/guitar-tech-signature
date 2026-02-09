38e47aa9ef02aefd2143b6e4338a23099e6515cf
Ken Kapptie <kkapptie@graniteschools.org>
2026-02-09 12:36:34 -0700

    Load and populate guitarist data from CSV

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+import { loadCSV } from "./dataLoader.js";
+
+const guitaristSelect = document.getElementById("guitaristSelect");
+
+async function init() {
+  const guitarists = await loadCSV("data/guitarists.csv");
+  populateGuitaristSelect(guitarists);
+}
+
+function populateGuitaristSelect(guitarists) {
+  guitarists.forEach(guitarist => {
+    const option = document.createElement("option");
+    option.value = guitarist.name;
+    option.textContent = guitarist.name;
+    guitaristSelect.appendChild(option);
+  });
+}
+
+init();
 
