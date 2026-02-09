a7b1c735341dc94e1313060d116651242e61779c
Ken Kapptie <kkapptie@graniteschools.org>
2026-02-09 12:59:35 -0700

    Update evaluator.js

--- js/evaluator.js
+++ js/evaluator.js
@@ -1,1 +1,17 @@
+export function evaluateTechniqueDistribution(sources, techniques) {
+  const tierCounts = {
+    Beginner: 0,
+    Intermediate: 0,
+    Advanced: 0
+  };
+
+  sources.forEach(entry => {
+    const tech = techniques.find(t => t.technique === entry.technique);
+    if (tech) {
+      tierCounts[tech.tier]++;
+    }
+  });
+
+  return tierCounts;
+}
 
